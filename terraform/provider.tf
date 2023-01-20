c53742bcf7065db7579fd5e80848de518d221645
Leo Pizarro <lpizarrobaeza@gmail.com>
2023-01-20 13:11:19 -0300

    adding google-beta provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,3 +14,8 @@ provider "google" {
   project     = var.project_name
   region      = var.gcp_region
 }
+
+provider "google-beta" {
+  project     = var.project_name
+  region      = var.gcp_region
+}
